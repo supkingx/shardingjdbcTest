01e6274fa1426baffffd5fb43ea2ad18210e6d4c
supkingx <supkingx@gmail.com>
2021-11-02 19:28:58 +0800

    修复启动问题

--- src/main/java/com/supkingx/Application.java
+++ src/main/java/com/supkingx/Application.java
@@ -3,8 +3,6 @@ package com.supkingx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @description:
@@ -12,9 +10,9 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @Date: 2021/11/1
  */
 @MapperScan("com.supkingx.dao")
-@SpringBootApplication(exclude = JtaAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
     public static void main(final String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
